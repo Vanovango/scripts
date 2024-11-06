4153e5e6b619770944833121f1a1089f35fa30c2
Ivan <voyten2003@bk.ru>
2024-11-06 21:35:18 +0300

    add test and val functions

--- change_image_size/change_image_size.py
+++ change_image_size/change_image_size.py
@@ -1,14 +1,27 @@
 from PIL import Image
 
 
-size = (2438, 1895)
+def for_all_images():
+    for i in range(1, 520 + 1):
+        if i % 10 == 0:
+            print(i)
+        tmp = str(i).zfill(3)
 
-for i in range(1, 520 + 1):
-    if i % 10 == 0:
-        print(i)
-    tmp = str(i).zfill(3)
+        im = Image.open(f'./images/Книга_105_Страница_{tmp}.jpg')
 
-    im = Image.open(f'./images/Книга_105_Страница_{tmp}.jpg')
-    im.thumbnail(size, Image.Resampling.LANCZOS)
-    im_crop_outside = im.crop((75, 75, 2_388, 1_845))
-    im_crop_outside.save(f'./книга/Книга_105_Страница_{tmp}.jpg', quality=95)
+        width, height = im.size
+        im_crop_outside = im.crop((65, 65, width - 65, height - 65))
+        im_crop_outside.save(f'./книга/Книга_105_Страница_{tmp}.jpg', quality=95)
+
+
+def test():
+    im = Image.open(f'./images/Книга_105_Страница_019.jpg')
+
+    width, height = im.size
+    im_crop_outside = im.crop((65, 65, width - 65, height - 65))
+    im_crop_outside.save(f'./книга/Книга_105_Страница_019.jpg', quality=95)
+
+
+if __name__ == "__main__":
+    for_all_images()
+    # test()
